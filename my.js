7bd5ad687f23ea94a70c5d6c64a807dc1fc58f42
csh8130 <csh8130@naver.com>
2019-11-30 02:51:44 +0900

    filter 함수

---
+++ my.js
@@ -0,0 +1,19 @@
+function addMaker(a) {
+  return function(b) {
+    return a + b;
+  };
+}
+
+/*
+    함수형 프로그래밍과 객체지향 프로그래밍의 차이는
+    객체를 확장하느냐 객체를 다루는 함수를 늘리느냐의 차이이며 추상화의 단위가 클래스이냐 함수이냐의 차이다.
+*/
+
+// predicate : 매개값을 조사해서 true 또는 false를 리턴하는 역할 출처: https://palpit.tistory.com/673
+function filter(list, predicate) {
+  var new_list = [];
+  for (var i = 0, len = list.length; i < len; i++) {
+    if (predicate(list[i])) new_list.push(list[i]);
+  }
+  return new_list;
+}
---
+++ test.js
@@ -0,0 +1,55 @@
+var users = [
+  { id: 1, name: "ID", age: 36 },
+  { id: 2, name: "BJ", age: 32 },
+  { id: 3, name: "JM", age: 32 },
+  { id: 4, name: "PJ", age: 27 },
+  { id: 5, name: "HA", age: 25 },
+  { id: 6, name: "JE", age: 26 },
+  { id: 7, name: "JI", age: 31 },
+  { id: 8, name: "MP", age: 23 }
+];
+
+// 1. 명령형 코드
+// 1. 30세 이상인 users를 거른다.
+var temp_users = [];
+for (var i = 0; i < users.length; i++) {
+  if (users[i].age >= 30) {
+    temp_users.push(users[i]);
+  }
+}
+console.log(temp_users);
+
+var temp_users = [];
+temp_users = filter(users, function(user) {
+  return user.age >= 30;
+});
+console.log(temp_users, "changed");
+
+// 2. 30세 이상인 users의 names를 수집한다.
+var names = [];
+for (var i = 0; i < temp_users.length; i++) {
+  names.push(temp_users[i].name);
+}
+console.log(names);
+
+// 3. 30세 미만인 users를 거른다.
+var temp_users = [];
+for (var i = 0; i < users.length; i++) {
+  if (users[i].age < 30) {
+    temp_users.push(users[i]);
+  }
+}
+console.log(temp_users);
+
+var temp_users = [];
+temp_users = filter(users, function(user) {
+  return user.age < 30;
+});
+console.log(temp_users, "changed");
+
+// 4. 30세 미만인 users의 ages를 수집한다.
+var ages = [];
+for (var i = 0; i < temp_users.length; i++) {
+  ages.push(temp_users[i].age);
+}
+console.log(ages);
